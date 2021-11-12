0c8c320dca7524e1ac23f5958c6cb8a5d7757fec
overkam <superivan0755@gmail.com>
2021-11-12 16:44:01 +0300

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Loader from './components/atoms/Loader';
 
 import Footer from './components/sections/Footer/index';
 import Header from './components/sections/Header/index';
@@ -8,13 +10,27 @@ import { animate } from './utils/animate';
 animate({ className: '.anim', animClassName: 'anim_active' });
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+    return () => clearTimeout(time);
+  }, []);
   return (
     <Router>
       <Route path="/">
         <div className="app">
-          <Header />
-          <LandingPage />
-          <Footer />
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <Header />
+              <LandingPage />
+              <Footer />
+            </>
+          )}
         </div>
       </Route>
       <div className="app-modals" />
